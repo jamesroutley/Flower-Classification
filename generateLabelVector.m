85056f15dc4c073d2ba78672ce453d9d298bd10b
James Routley <jroutley@gmail.com>
2014-11-03 17:20:08 +0000

    Uses full set as training, rotated for test (17)

--- generateLabelVector.m
+++ generateLabelVector.m
@@ -1,7 +1,7 @@
 function labelVector = generateLabelVector(numberOfPhotos, modelNum)
 % generates the lableVector used by svmtrain and svmpredict
 
-a = -ones(numberOfPhotos,40);
+a = zeros(numberOfPhotos,80);
 a(modelNum, :) = +1;
 a = reshape(a', 1, []);
 labelVector = a';
