c76ebb0f3e84e9c4f78c2c9641e8a355b5e63ff9
jamesroutley <admin@Stockholm.home>
2014-11-08 13:22:08 +0000

    Revert f898030..94f8991
    This rolls back to commit f898030118f6ab2e4a3d2e348b8424040a8dcc1b.

--- generateLabelVector.m
+++ generateLabelVector.m
@@ -1,7 +1,7 @@
 function labelVector = generateLabelVector(numberOfPhotos, modelNum)
 % generates the lableVector used by svmtrain and svmpredict
 
-a = zeros(numberOfPhotos,80);
+a = -ones(numberOfPhotos,40);
 a(modelNum, :) = +1;
 a = reshape(a', 1, []);
 labelVector = a';
--- jr_cnn_script.m
+++ jr_cnn_script.m
@@ -2,53 +2,76 @@
 %and saves the returned feature vector
 
 % initialise variables
-flowerSetNumber = 17;
-flowerSetNumberLable = num2str(flowerSetNumber);
+flowerSetNumber = 5;
 numberOfImagesPerFlower = 80;
-imageFolder = 'oxfordflower17/';
-imageTrainFolder = strcat(imageFolder, 'jpgtrain/');
-imageTestFolder = strcat(imageFolder, 'jpgtest/');
+imageFolder = 'oxfordflower5';
 numTotalImages = flowerSetNumber * numberOfImagesPerFlower;
-numTrainingImages = numTotalImages;
-numTestImages = numTotalImages;
+numTrainingImages = numTotalImages/2;
+numTestImages = numTotalImages/2;
 
 
 % import vector of flower file names
-imageName = importdata(strcat(imageFolder,'files.txt'));
+imageName = importdata(strcat(imageFolder,'/jpg/files.txt'));
 imageName = cell2mat(imageName);
 
 % generate vector of image categorisation labels
-imageLabels = load(strcat(imageFolder,'labels.mat'));
+imageLabels = load(strcat(imageFolder,'/labels.mat'));
 imageLabels = (cell2mat(struct2cell(imageLabels)));
 
-% for simplified 5 flower case only:
+% for simplified 3 flower case only:
 if flowerSetNumber == 5
     imageLabels = imageLabels(1:numTotalImages);
 end
 
 
+% generate vectors containing the indeces of training and testing data
+trainingIndexVector = ones(1, numTrainingImages);
+testIndexVector = ones(1, numTestImages);
+trainingCount = 0;
+testCount = 0;
+flag = 1;
+for i = 1:numTotalImages %size(imageLabels, 2)
+   
+   if flag == 1 
+       trainingCount = trainingCount + 1;
+       trainingIndexVector(trainingCount) = i;
+   end
+   
+   if flag == -1 
+       testCount = testCount + 1;
+       testIndexVector(testCount) = i;
+   end
+   
+   if mod(i, 40) == 0
+       flag = flag * -1;
+   end
+   
+end
+
+
+
 % load / generate trainingInstanceMatrix storing training flower feature data
-if exist(strcat(imageFolder,'trainingInstanceMatrix', flowerSetNumberLable, '.mat'))
-    trainingInstanceMatrix = load(strcat(imageFolder,'trainingInstanceMatrix', flowerSetNumberLable, '.mat'));
+if exist(strcat(imageFolder,'/trainingInstanceMatrix.mat'))
+    trainingInstanceMatrix = load(strcat(imageFolder,'/trainingInstanceMatrix.mat'));
     trainingInstanceMatrix = (cell2mat(struct2cell(trainingInstanceMatrix)));
 else
-    trainingInstanceMatrix = ones(numTrainingImages, 4096);
-    for i = 1 : numTrainingImages
-        trainingInstanceMatrix(i, :) = jr_cnn(imageName(i , :), imageTrainFolder);
+    trainingInstanceMatrix = ones(size(trainingIndexVector, 2), 4096);
+    for i = 1 : size(trainingIndexVector, 2)
+        trainingInstanceMatrix(i, :) = jr_cnn(imageName(trainingIndexVector(i), :), imageFolder);
     end
-    save(strcat(imageFolder,'trainingInstanceMatrix', flowerSetNumberLable, '.mat'), 'trainingInstanceMatrix');
+    save(strcat(imageFolder,'/trainingInstanceMatrix.mat'), 'trainingInstanceMatrix');
 end
 
 % load / generate testInstanceMatrix storing test flower feature data
-if  exist(strcat(imageFolder,'testInstanceMatrix', flowerSetNumberLable, '.mat'))
-    testInstanceMatrix = load(strcat(imageFolder,'testInstanceMatrix', flowerSetNumberLable, '.mat'));
+if  exist(strcat(imageFolder,'/testInstanceMatrix.mat'))
+    testInstanceMatrix = load(strcat(imageFolder,'/testInstanceMatrix.mat'));
     testInstanceMatrix = (cell2mat(struct2cell(testInstanceMatrix)));
 else
-    testInstanceMatrix = ones(numTestImages, 4096);
-    for i = 1 : numTestImages
-        testInstanceMatrix(i, :) = jr_cnn(imageName(i, :), imageTestFolder);
+    testInstanceMatrix = ones(size(trainingIndexVector, 2), 4096);
+    for i = 1 : size(trainingIndexVector, 2)
+        testInstanceMatrix(i, :) = jr_cnn(imageName(testIndexVector(i), :), imageFolder);
     end
-    save(strcat(imageFolder,'testInstanceMatrix', flowerSetNumberLable, '.mat'), 'testInstanceMatrix')
+    save(strcat(imageFolder,'/testInstanceMatrix.mat'), 'testInstanceMatrix')
 end
 
 
